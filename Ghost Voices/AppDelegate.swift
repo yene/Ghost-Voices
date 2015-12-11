32da4c7d137ebc9959210263807a32235a08271f
yene <yene@slowmouse.ch>
2015-12-11 15:02:26 +0100

    Initial Commit

---
+++ Ghost Voices/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Ghost Voices
+//
+//  Created by Yannick Weiss on 11/12/15.
+//  Copyright © 2015 Yannick Weiss. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
